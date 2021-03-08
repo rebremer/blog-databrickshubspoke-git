81a5646828dbb49a9be271b545369a9d790d2f1f
Rene Bremer <rebremer@microsoft.com>
2021-03-08 18:03:25 +0100

    dos2unix

--- scripts/0_run_script.sh
+++ scripts/0_run_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./1_deploy_storage_1_hub.sh
-./2_deploy_databricks_N_spokes.sh
-./3_configure_network_N_spokes.sh
-./4_mount_storage_N_spokes.sh
+#./2_deploy_databricks_N_spokes.sh
+#./3_configure_network_N_spokes.sh
+#./4_mount_storage_N_spokes.sh
